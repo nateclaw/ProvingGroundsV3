cfa3605f30f14a1e9c18ef9721a59f1c7a9e655b
Nate L <leimern1978@my.uwstout.edu>
2026-07-10 20:30:38 -0500

    Updated to 5.8

--- Source/ProvingGroundsV3Editor.Target.cs
+++ Source/ProvingGroundsV3Editor.Target.cs
@@ -8,7 +8,7 @@ public class ProvingGroundsV3EditorTarget : TargetRules
 	public ProvingGroundsV3EditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("ProvingGroundsV3");
 	}
